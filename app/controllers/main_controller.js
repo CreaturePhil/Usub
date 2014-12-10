94ecb956215979336d1f581411415e9ae4d316d4
Phil <birkal@outlook.com>
2014-12-09 19:52:28 -0800

    Update static scraper

--- app/controllers/main_controller.js
+++ app/controllers/main_controller.js
@@ -16,17 +16,18 @@ module.exports = {
     async.each(req.user.subscriptions, function(user, callback) {
       var link = yt_channel_startLink + user + yt_channel_endLink;
       request(link, function(err, response, body) {
-        if (err || response.statusCode !== 200) return callback();
+        if (err || response.statusCode !== 200) return callback(); 
         var $ = cheerio.load(body);
         var content = $('.channels-content-item').map(function(i, el) {
+          var $info = $(this).find('.yt-lockup-meta-info').find('li');
           return {
             img: 'https:' + $(this).find('img').attr('src'),
             link: 'https://www.youtube.com' + $(this).find('a').attr('href').split('&')[0],
             time: $(this).find('.video-time').text(),
             title: $(this).find('.yt-lockup-content a').text(),
             author: user,
-            viewCount: $(this).find('.yt-lockup-meta ul li').first().text(),
-            publishedAt: $(this).find('.yt-lockup-deemphasized-text').text()
+            viewCount: $info.first().text(),
+            publishedAt: $info.last().text()
           };
         }).get();
         videos = videos.concat(content);
